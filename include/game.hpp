8257871a4c00342b13b32aa1671974a7c6608875
Akhdan Hilmi <akhdanhilmi.m@gmail.com>
2026-06-11 03:14:13 +0700

    refactor: rename app files to coreApps for future modular scalability
    chore: update build configuration in CMakeLists.txt

--- include/game.hpp
+++ include/game.hpp
@@ -4,7 +4,7 @@
 #include <SFML/Graphics.hpp>
 #include "Desktop.hpp"
 #include "GlitchManager.hpp"
-#include "Apps.hpp"
+#include "coreApps.hpp"
 
 class Game {
 private:
--- src/coreApps.cpp
+++ src/coreApps.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Apps.hpp"
+#include "coreApps.hpp"
 
 //NOTEPAD APP
 
